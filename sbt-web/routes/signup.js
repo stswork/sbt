e9ab405d65f28c2324de07b0032543fd1ad8ca84
Sagar Gopale <sagar090989@yahoo.com>
2014-08-19 23:26:26 +0530

    Fix in signup url

--- sbt-web/routes/signup.js
+++ sbt-web/routes/signup.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/signup', function(req, res) {
-  res.render('signup.html', { title: 'SignUp' });
+router.get('/', function(req, res) {
+  res.render('signup', { title: 'SignUp' });
 });
 
 module.exports = router;
